7a9b695141260cb25028767cddc26e0d3a44bc6f
mdasifhosen <mdasifhosen2028@gmail.com>
2025-08-29 22:46:27 +0600

    added an cal section and history

--- js/index.js
+++ js/index.js
@@ -1,192 +1,264 @@
-document.getElementById('heart-icon1').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon2').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon3').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon4').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon5').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon6').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon7').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon8').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
-document.getElementById('heart-icon9').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("love-btn");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current + 1;
-})
+document.getElementById("heart-icon1").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon2").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon3").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon4").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon5").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon6").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon7").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon8").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
+document.getElementById("heart-icon9").addEventListener("click", function (e) {
+  e.preventDefault();
+  let loveCount = document.getElementById("love-btn");
+  let current = parseInt(loveCount.innerText);
+  loveCount.innerText = current + 1;
+});
 
 // call setion
 
+const historyData = [];
+document.getElementById("call-1").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+ 
+  if (current < 20) {
+    alert("not coin");
+     return;
+  } else {
+    alert("calling National Emergency Number 999.. ");
+    
+  }
+ 
 
-document.getElementById('call-1').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-2').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-3').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-4').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-5').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-6').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-7').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-8').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
-document.getElementById('call-9').addEventListener('click', function (e) {
-    e.preventDefault();
-    let loveCount = document.getElementById("coin");
-    let current = parseInt(loveCount.innerText);
-    loveCount.innerText = current - 20;
-    alert('calling National Emergency Number 999.. ')
-})
+  
+});
 
+
+document.getElementById("call-2").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+    if (current < 20) {
+      alert("not coin");
+      return;
+    } else {
+      alert("calling National Emergency Number 999.. ");
+    }
+});
+
+document.getElementById("call-3").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+    if (current < 20) {
+      alert("not coin");
+      return;
+    } else {
+      alert("calling National Emergency Number 999.. ");
+    }
+});
+document.getElementById("call-4").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+    if (current < 20) {
+      alert("not coin");
+      return;
+    } else {
+      alert("calling National Emergency Number 999.. ");
+    }
+});
+document.getElementById("call-5").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+     if (current < 20) {
+       alert("not coin");
+       return;
+     } else {
+       alert("calling National Emergency Number 999.. ");
+     }
+});
+document.getElementById("call-6").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+     if (current < 20) {
+    alert("not coin");
+     return;
+  } else {
+    alert("calling National Emergency Number 999.. ");
+    
+  }
+   
+});
+document.getElementById("call-7").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+     if (current < 20) {
+       alert("not coin");
+       return;
+     } else {
+       alert("calling National Emergency Number 999.. ");
+     }
+});
+document.getElementById("call-8").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+     if (current < 20) {
+       alert("not coin");
+       return;
+     } else {
+       alert("calling National Emergency Number 999.. ");
+     }
+});
+document.getElementById("call-9").addEventListener("click", function (e) {
+  e.preventDefault();
+  let callCount = document.getElementById("coin");
+  let current = parseInt(callCount.innerText);
+  callCount.innerText = current - 20;
+     if (current < 20) {
+       alert("not coin");
+       return;
+     } else {
+       alert("calling National Emergency Number 999.. ");
+     }
+});
 
 // copy section
 document.getElementById("copy-1").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-2").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-3").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-4").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-5").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-6").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-7").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-8").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 document.getElementById("copy-9").addEventListener("click", function (e) {
   e.preventDefault();
-  let loveCount = document.getElementById("copy");
-  let current = parseInt(loveCount.innerText);
-  loveCount.innerText = current +1;
+  let copyCount = document.getElementById("copy");
+  let current = parseInt(copyCount.innerText);
+  copyCount.innerText = current + 1;
   alert(" Number has been copied successfully 999.. ");
 });
 
+// history section
+function addToHistory(name, number) {
+  const historyBox = document.getElementById("history-containers");
+  const timeNow = new Date().toLocaleTimeString();
 
+  const card = document.createElement("div");
+  card.className = "rounded-[8px] w-[250px] h-[100px] bg-[#f5fff6] m-[24px]";
 
+  card.innerHTML = `
+    <div class="p-[10px] flex justify-between items-center">
+      <div>
+        <h1 class="font-bold">${name}</h1>
+        <p class="text-[#5C5C5C]">${number}</p>
+      </div>
+      <div>
+        <span>${timeNow}</span>
+      </div>
+    </div>
+  `;
 
-
+  historyBox.prepend(card);
+}
